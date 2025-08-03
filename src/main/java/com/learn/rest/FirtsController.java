b351a6cf71949862f6d6ecda844afba3bf0cb244
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2025-08-03 14:34:21 +0100

    feat: 🎸 first dto

--- src/main/java/com/learn/rest/FirtsController.java
+++ src/main/java/com/learn/rest/FirtsController.java
@@ -1,6 +1,8 @@
 package com.learn.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class FirtsController {
     @GetMapping("hey")
     public String hey() {
         return "Hey from contoller";
+    }
+
+    @PostMapping("post-order")
+    public String postText(@RequestBody OrderDTO data) {
+        return data.toString();
     }
 
 }
---
+++ src/main/java/com/learn/rest/OrderDTO.java
@@ -0,0 +1,38 @@
+package com.learn.rest;
+
+public class OrderDTO {
+
+    private String customerName;
+    private String productName;
+    private int quantity;
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return customerName + " " + productName + " " + quantity;
+    }
+
+}
